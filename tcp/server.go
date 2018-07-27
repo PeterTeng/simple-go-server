cf0ad18d8ad573b7c2475c1c76afcfcbf41a8f94
DONGSHIH TENG <tou9702@gmail.com>
2018-07-27 13:49:16 +0900

    TCP server

---
+++ tcp/server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+  "fmt"
+  "net"
+  "os"
+)
+
+const (
+  connectionHost = "localhost"
+  connectionPort = "8888"
+  connectionType = "tcp"
+  connectionAddr = connectionHost + ":" + connectionPort
+)
+
+func main() {
+  l, err := net.Listen(connectionType, connectionAddr)
+  if err != nil {
+    fmt.Println("Error listening:", err.Error())
+    os.Exit(1)
+  }
+
+  defer l.Close()
+
+  fmt.Println("Listening on " + connectionAddr)
+
+  for {
+    conn, err := l.Accept()
+    if err != nil {
+      fmt.Println("Error accepting:", err.Error())
+      os.Exit(1)
+    }
+
+    go handleRequest(conn)
+  }
+}
+
+func handleRequest(conn net.Conn) {
+  buf := make([]byte, 1024)
+
+  _, err := conn.Read(buf)
+
+  if err != nil {
+    fmt.Println("Error reading:", err.Error())
+  }
+  conn.Write([]byte("Message received.\n"))
+  conn.Close()
+}
